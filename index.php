30709d42ee0430055d6773c1ac5ef9560aa07000
heroes618 <heroes618@hotmail.com>
2015-04-17 08:50:52 -0700

    video 11

--- index.php
+++ index.php
@@ -27,12 +27,12 @@
 
 				if(new_task !=''){
 					$.post('includes/add-task.php', {task: new_task}, function(data){
-						$(('add-new-task input[name=new-task]').val();
+						$('add-new-task input[name=new-task]').val();
 							$(data).appendTo('task-list ul').hide().fadeIn();
 					});
 				}
 				return false;
-		})
+		});
 	}
 </script>
 
