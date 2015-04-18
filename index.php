dde7261893f7bae87847350414c3c4d04014e80b
heroes618 <heroes618@hotmail.com>
2015-04-18 01:31:13 -0700

    video 14

--- index.php
+++ index.php
@@ -34,6 +34,17 @@
 				return false;
 		});
 	}
+
+	$('.delete-button').click(function()){// when you click this we want is function
+	var current_element = $(this);
+	var task_id = $(this).attr('id');
+
+	$.post('includes/delete-task.php', {id: task_id}, function()) 
+		current_element.parent().fadeOut("fast", function(){
+			$(this).remove();
+		});
+	});
+});
 </script>
 
  </html>		
