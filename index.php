8818e6933952ccbf18cb3250d2cc0afdb131c7f7
heroes618 <heroes618@hotmail.com>
2015-04-21 08:37:46 -0700

    little error fix

--- index.php
+++ index.php
@@ -2,25 +2,25 @@
  <html>
  <head>
  	<title> Mom's To do list</title>
-	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" href="css/main.css">
 </head>
  <body>
  	<div class="wrap">
  		<div class="task-list">
  			<ul>
  				<?php require("includes/connect.php"); 
- 				$mysqli = new mysqli('localhost', 'root','root','tasks');
- 				$query = "SELECT = FROM tasks ORDER BY date ASC, time ASC ";
+ 				$mysqli = new mysqli('localhost', 'root','root','todo');
+ 				$query = "SELECT * FROM tasks ORDER BY date ASC, time ASC ";
  				if ($result = $mysqli->query($query)) {
  					$numrows = $result->num_rows;
  					if ($numrows>0) {
  						while($row = $result->fetch_assoc()){
  							$task_id = $row['id'];
- 							$task_name = $row["task"];
+ 							$task_name = $row['task'];
 
  							echo '<li>
  							<span>'. $task_name.'</span>
- 							<img id="'.$task_id.'" class="delete-button" width="10px" src="images/close.svg"/>
+ 							<img id="'.$task_id.'"" class="delete-button" width="10px" src="images/close.svg"/>
  							</li>';
  						}
  					}
@@ -28,36 +28,33 @@
  				}
 				?>
  			</ul>
- 		</div>	
- 	<form class="add-new-task" autocomplet="off">
- 		<input type="text" name="new-task" placeholder="Add new item..."/>
- 		
- 	</form>
- 	</div>	
- </body>
-<script type="http://code.jquery.com/jquery-lastest.min.js"></script>
+</div>
+	<form class="add-new-task" autocomplete="off">
+<input type="text" name="new-task" placeholder="Add new item..."/>
+</form>
+</div>
+</body>
+<script src="https://code.jquery.com/jquery-latest.min.js"></script>
 <script>
 	add_task(); //calling the add task function
-
-	function add_task(){
-		$('.add-new-task').submit(function() {
-			var new_task = $('.add-new-task input[name=new-task]').val();
-
-				if(new_task !=''){
-					$.post('includes/add-task.php', {task: new_task}, function(data){
-						$('add-new-task input[name=new-task]').val();
-							$(data).appendTo('.task-list ul').hide().fadeIn();
-					});
-				}
+function add_task(){
+	$(".add-new-task").submit(function(){
+var new_task = $(".add-new-task input[name=new-task]").val();
+if(new_task !== ""){
+	$.post("includes/add-task.php", {task: new_task}, function(data){
+$("add-new-task input[name=new-task]").val();
+	$(data).appendTo(".task-list ul").hide().fadeIn();
+	});
+}
 				return false;
 		});
 	}
 
-	$('.delete-button').click(function()){// when you click this we want is function
+	$('.delete-button').click(function(){// when you click this we want is function
 	var current_element = $(this);
 	var task_id = $(this).attr('id');
 
-	$.post('includes/delete-task.php', {id: task_id}, function()) 
+	$.post('includes/delete-task.php', {id: task_id}, function(){
 		current_element.parent().fadeOut("fast", function(){
 			$(this).remove();
 		});
